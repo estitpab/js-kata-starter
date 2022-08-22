390ae39afd4bee5de94251827756e1c3a1dcae30
Baptiste <bnonat@mkdirect.fr>
2022-08-22 17:56:34 +0200

    Add fraction exercice

--- src/fractions/index.spec.ts
+++ src/fractions/index.spec.ts
@@ -16,3 +16,54 @@ test("Addition de fraction nulle", () => {
 
   expect(addFraction(fraction1, fraction2)).toEqual(result);
 });
+
+test("Addition de fraction denominateur commun", () => {
+  const fraction1:Fraction = {
+    numerator: 1,
+    denominator: 3,
+  };
+  const fraction2:Fraction = {
+    numerator: 1,
+    denominator: 3,
+  };
+  const result:Fraction = {
+    numerator: 2,
+    denominator: 3,
+  };
+
+  expect(addFraction(fraction1, fraction2)).toEqual(result);
+});
+
+test("Addition de fraction denominateur différent", () => {
+  const fraction1:Fraction = {
+    numerator: 1,
+    denominator: 2,
+  };
+  const fraction2:Fraction = {
+    numerator: 1,
+    denominator: 3,
+  };
+  const result:Fraction = {
+    numerator: 5,
+    denominator: 6,
+  };
+
+  expect(addFraction(fraction1, fraction2)).toEqual(result);
+});
+
+test("Réduire la fraction", () => {
+  const fraction1:Fraction = {
+    numerator: 3,
+    denominator: 9,
+  };
+  const fraction2:Fraction = {
+    numerator: 3,
+    denominator: 9,
+  };
+  const result:Fraction = {
+    numerator: 2,
+    denominator: 3,
+  };
+
+  expect(addFraction(fraction1, fraction2)).toEqual(result);
+});
--- src/fractions/index.ts
+++ src/fractions/index.ts
@@ -2,9 +2,28 @@ export type Fraction = {
     numerator:number,
     denominator:number,
 }
+function scale(fraction1:Fraction, factor:number):Fraction {
+  const numerator = fraction1.numerator * factor;
+  const denominator = fraction1.denominator * factor;
+  return { numerator, denominator };
+}
+
+function gcd(a, b) {
+  return b === 0 ? a : gcd(b, a % b);
+}
+
+function reduction(fraction: Fraction): Fraction {
+  const greatestCommonDivisor = gcd(fraction.numerator, fraction.denominator);
+  return { numerator: fraction.numerator / greatestCommonDivisor, denominator: fraction.denominator / greatestCommonDivisor };
+}
 
 function addFraction(fraction1, fraction2):Fraction {
-  return { numerator: 1, denominator: 2 };
+  if (fraction1.denominator !== fraction2.denominator) {
+    const scaledFraction1 = scale(fraction1, fraction2.denominator);
+    const scaledFraction2 = scale(fraction2, fraction1.denominator);
+    return reduction({ numerator: (scaledFraction1.numerator + scaledFraction2.numerator), denominator: scaledFraction1.denominator });
+  }
+  return reduction({ numerator: (fraction1.numerator + fraction2.numerator), denominator: fraction1.denominator });
 }
 
 export default addFraction;
